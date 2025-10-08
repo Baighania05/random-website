74b9ccdae135af91003c921c24f749b49448e225
Baighania05 <baighania05@gmail.com>
2025-10-08 19:48:19 +0500

    new created file

---
+++ first.js
@@ -0,0 +1,33 @@
+document.write('hey!');
+// var
+// let 2015
+// const
+// snytax
+// let var const create a varaibale hum variable bana rhy
+var stdName ="arshma";
+var stdName ="hiba";
+
+document.write(stdName);
+// camel case
+
+let StudentName="Hania";
+// same name s var mhi bana sakty lihkn you can change the value of the variable
+Studentname ="Raffy";
+document.write(StudentName);
+
+
+        //  same variable you can't change its value age =22;
+
+        document.write(age);
+
+        // build in function in js
+        alert("Thankyou for visiting our site"); //pop up generate
+        // data save=> variable
+        // prompt("enter your name?")
+        let name =prompt("Enter your name ?")  //user input answer save
+        document.write(name);
+
+        let b = prompt ("enter your age")
+        document.write(b)
+
+        
